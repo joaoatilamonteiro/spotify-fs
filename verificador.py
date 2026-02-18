d6d3ad2e28d5a037684ac77c3bc7e58295a62652
Adomo <steeljaw2006@gmail.com>
2026-02-18 11:15:45 -0300

    Criação da função gerar_tabela. Em que 256 musicas são escolhidas para representar cada valor possivel de um byte

--- verificador.py
+++ verificador.py
@@ -3,6 +3,7 @@ from spotify_cliente import spotifyclient
 
 class verificador:
     def __init__(self, client,senha):
+        self.client = client
         #aqui ele criptografa a senha em hash hexadecimal
         hash_senha = hashlib.sha256(senha.encode("utf-8")).hexdigest()
 
@@ -13,3 +14,19 @@ class verificador:
         random.seed(semente)
 
     def gerar_tabela(self):
+        tabela_musicas = []
+        #musicas que ja foram usadas vao ser guardadas nesse set
+        musicas_usadas = set()
+        variavel_string = "abcdefghijklmnopqrstuvwxyz"
+        while len(tabela_musicas) < 256:
+            letra_sorteada = random.choice(variavel_string)
+            resultados = self.client.procura_musica(letra_sorteada)
+            for musica in resultados:
+                uri_musica = musica["uri"]
+                if uri_musica not in musicas_usadas:
+                    tabela_musicas.append(uri_musica)
+                    musicas_usadas.add(uri_musica)
+                    if len(tabela_musicas) == 256: break
+
+        return tabela_musicas
+
