5861653cdab638fb5dca6e336904d9f204db81c0
Adomo <steeljaw2006@gmail.com>
2026-02-18 11:30:28 -0300

    adicionando imports

---
+++ imports.py
@@ -0,0 +1,9 @@
+import time
+import urllib.parse
+import json
+import requests
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import hashlib #transforma a senha em hash
+import random
+import string
